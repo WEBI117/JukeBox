e9a092c84bde3317ec1e8c316074a2f481eeba97
Webi <wahidejaz@hotmail.com>
2019-05-15 16:58:53 +0500

    integrated socket io and react navigation with proper statemanagement and server cient communication.

--- App.js
+++ App.js
@@ -8,43 +8,65 @@ import io from 'socket.io-client'
 
 
 class screen1 extends React.Component {
-  
-  componentDidMount(){
-    const socket = io('http://10.130.12.76:3000')
-
-    socket.on('message',() => {})
+  constructor(props){
+    super(props)
+    const socket = io('http://192.168.100.23:3000')
+    this.state = {
+      sock: socket,
+      input : '',
+      arr: ''
+    }
   }
-  
+
+  textinputhandler = (val) => {
+    this.setState({
+      input:val
+    })
+  }
+  buttonhandler = () => {
+    this.state.sock.emit('message', this.state.input)
+    prom = new Promise ((resolve,reject) => {
+      this.state.sock.on('hi', (v) => {
+          resolve(v)
+        })
+    })
+    .then((v) => {
+      this.props.navigation.navigate('Screen2', {
+      ssocket: this.state.sock,
+      sermessage: v
+    })
+  })
+    .catch(() => {alert('error')})
+
+  }
+ 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Button
-          title = "screen2"
-          onPress={() => alert('hello')}
-        />
+      <View style={styles.container}>
+          <TextInput
+            style = {{width:200}}
+            placeholder = 'search'
+            value = {this.state.input}
+            onChangeText = {this.textinputhandler}
+          />
+          <Button
+            title = 'submit'
+            onPress = {this.buttonhandler}
+          />
       </View>
     );
   }
 }
 
 class screen2 extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      v: 'aaaa',
-    };
-  }
-  componentDidMount(){
-    const { navigation } = this.props;
-    const val = navigation.getParam('val', 'none');
-    this.setState({
-      v: val
-    })
-  }
   render() {
+    const { navigation } = this.props;
+    const s = navigation.getParam('ssocket');
+    const sm = navigation.getParam('sermessage','woops');
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>{this.state.v}</Text>
+      <View style={styles.container}>
+        <Text>screen 2!!</Text>
+        <Text>{sm}</Text>
       </View>
     );
   }
@@ -67,26 +89,11 @@ export default class App extends Component {
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
-// fc = (val) => {
-//   this.setState({
-//     value: val
-//   })
-// }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+})
